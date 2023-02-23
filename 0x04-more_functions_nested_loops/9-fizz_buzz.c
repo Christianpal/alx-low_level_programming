37ecb433bb730a5552b149a9c5da95a7874627ec
christianpal <godfreypal@gmail.com>
2023-02-23 12:29:52 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,23 +18,23 @@ int main(void)
 
 	while (i < 101)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz");
-		}
-		else
-		{
-			printf("%d", i);
-		}
-		i++;
+	if (i % 3 == 0 && i % 5 == 0)
+	{
+	printf("FizzBuzz");
+	}
+	else if (i % 3 == 0)
+	{
+	printf("Fizz");
+	}
+	else if (i % 5 == 0)
+	{
+	printf("Buzz");
+	}
+	else
+	{
+	printf("%d", i);
+	}
+	i++;
 	}
 	printf("\n");
 	return (0);
