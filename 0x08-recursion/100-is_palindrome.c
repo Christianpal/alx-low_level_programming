cc796acb44056c98073fab65e92eb10291bb623e
christianpal <godfreypal@gmail.com>
2023-03-08 13:58:02 -0800

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,9 +2,10 @@
 
 /**
 * _strlen_recursion - returns the length og a string
-@ s: string
+* @s: string
 * Return: the length of a string
 */
+
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
@@ -24,7 +25,7 @@ int comparator(char *s, int n1, int n2)
 {
 	if (*(s + n1) == *(s + n2))
 	{
-		if (n1 == n2 || n1 == n2 +1)
+		if (n1 == n2 || n1 == n2 + 1)
 			return (1);
 		return (0 + comparator(s, n1 + 1, n2 - 1));
 	}
